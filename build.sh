34c158602c6266cc2f178891adb3e90441280344
Alex Gaynor <alex.gaynor@gmail.com>
2016-01-26 13:18:03 -0500

    mpi

--- build.sh
+++ build.sh
@@ -26,6 +26,7 @@ gcc parse.c kshim.c \
     $LINUX_LOCATION/crypto/memneq.o \
     $LINUX_LOCATION/crypto/asymmetric_keys/{asymmetric_type,public_key,rsa,x509_cert_parser,x509-asn1,x509_akid-asn1,x509_rsakey-asn1}.o \
     $LINUX_LOCATION/lib/{asn1_decoder,hexdump}.o \
+    $LINUX_LOCATION/lib/mpi/{mpi-bit,mpi-cmp,mpi-pow,mpicoder,mpiutil,mpih-cmp,mpih-div,mpih-mul,generic_mpih-add1,generic_mpih-lshift,generic_mpih-mul1,generic_mpih-mul2,generic_mpih-mul3,generic_mpih-rshift,generic_mpih-sub1}.o \
     2>&1 | grep "undefined reference"  | grep -E -o "\`([a-zA-Z0-9_]*)'" | sort | uniq -c | sort -rn
 
 # gcc parse.c kshim.c /vagrant/linux/crypto/memneq.o /vagrant/linux/crypto/asymmetric_keys/{asymmetric_type,public_key,rsa,x509_cert_parser,x509-asn1,x509_akid-asn1,x509_rsakey-asn1}.o /vagrant/linux/lib/asn1_decoder.o  /vagrant/linux/lib/mpi/{mpi-bit,mpi-cmp,mpi-pow,mpicoder,mpiutil,mpih-cmp,mpih-div,mpih-mul,generic_mpih-add1,generic_mpih-lshift,generic_mpih-mul1,generic_mpih-mul2,generic_mpih-mul3,generic_mpih-sub1,generic_mpih-rshift}.o
