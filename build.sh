4b240b64a6696e0cd8eafb4f93e5ea538e46996c
Alex Gaynor <alex.gaynor@gmail.com>
2016-01-26 11:50:19 -0500

    Start linking in kshim.c

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ echo "CONFIG_ASYMMETRIC_KEY_TYPE=y" >> .config
 echo "CONFIG_ASYMMETRIC_PUBLIC_KEY_SUBTYPE=y" >> .config
 echo "CONFIG_PUBLIC_KEY_ALGO_RSA=y" >> .config
 echo "CONFIG_X509_CERTIFICATE_PARSER=y" >> .config
+echo "CONFIG_PKCS7_MESSAGE_PARSER=n" >> .config
 make silentoldconfig
 
 export MAKE_JOBS=3
@@ -21,6 +22,6 @@ make SUBDIRS=crypto/asymmetric_keys/
 make SUBDIRS=lib/
 
 cd $KFUZZ_LOCATION
-gcc parse.c $LINUX_LOCATION/crypto/asymmetric_keys/x509_cert_parser.o 2>&1 | grep "undefined reference"  | grep -E -o "\`([a-zA-Z0-9_]*)'" | sort | uniq -c | sort -rn
+gcc parse.c kshim.c $LINUX_LOCATION/crypto/asymmetric_keys/x509_cert_parser.o 2>&1 | grep "undefined reference"  | grep -E -o "\`([a-zA-Z0-9_]*)'" | sort | uniq -c | sort -rn
 
-# gcc parse.c kshim.c /vagrant/linux/crypto/memneq.o /vagrant/linux/crypto/asymmetric_keys/{asymmetric_type,public_key,rsa,x509_cert_parser,x509-asn1,x509_akid-asn1,x509_rsakey-asn1}.o /vagrant/linux/lib/asn1_decoder.o  /vagrant/linux/lib/mpi/{mpi-bit,mpi-cmp,mpi-pow,mpicoder,mpiutil,mpih-cmp,mpih-div,mpih-mul,generic_mpih-add1,generic_mpih-lshift,generic_mpih-mul1,generic_mpih-mul2,generic_mpih-mul3,generic_mpih-sub1,generic_mpih-rshift}.o 2>&1  | grep "undefined reference"  | grep -E -o "\`([a-zA-Z0-9_]*)'" | sort | uniq -c | sort -rn
+# gcc parse.c kshim.c /vagrant/linux/crypto/memneq.o /vagrant/linux/crypto/asymmetric_keys/{asymmetric_type,public_key,rsa,x509_cert_parser,x509-asn1,x509_akid-asn1,x509_rsakey-asn1}.o /vagrant/linux/lib/asn1_decoder.o  /vagrant/linux/lib/mpi/{mpi-bit,mpi-cmp,mpi-pow,mpicoder,mpiutil,mpih-cmp,mpih-div,mpih-mul,generic_mpih-add1,generic_mpih-lshift,generic_mpih-mul1,generic_mpih-mul2,generic_mpih-mul3,generic_mpih-sub1,generic_mpih-rshift}.o
